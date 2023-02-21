18be0cdb76810a5793f48430933bdb4365519131
Mtab <107020397+DamenaM@users.noreply.github.com>
2023-02-21 20:43:37 +0300

    Add meals list

--- src/Modules/display.js
+++ src/Modules/display.js
@@ -2,14 +2,28 @@ const scoreDisplay = document.querySelector('.tableBody');
 const displayScores = (arr) => {
   scoreDisplay.innerHTML = '';
   const displayScores = arr.map(
-    (item) => `
-          <tr >
-          <td class="score-container" ><img class="imagemeals" src="${item.strMealThumb}"></td>
-          <td class="score-container" ><span>Meals-Tags:${item.strTags} </span></td>
-          <td class="score-container" ><span>MealCategory:${item.strCategory}</span></td>
-          <td class="score-container" ><span>Meal-KG=:${item.strMeasure1} </span></td>
 
-        </tr>`,
+    (item) => `
+
+        <section class="features">
+    <div class="feature">
+        <div class="feature-img">
+            <img class="imagemeals" src="${item.strMealThumb}" alt="">
+        </div>
+        <div class="info">
+            <h3>${item.strTags}</h3>
+            <p class="hardware">${item.strCategory}</p>
+            <div class="line-feature"></div>
+            <h4>${item.strMeasure1} </h4>
+            <div class="btn">
+            <button id="refreshScores"  type="button" > Comment</button>
+            <br>
+            <button id="refreshScores"  type="button" > reservation</button>
+            </div>
+        </div>
+    </div>
+</section>`
+    ,
   );
   scoreDisplay.innerHTML = displayScores.join(' ');
 };
