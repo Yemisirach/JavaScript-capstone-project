5102aacda601d52700ed1545afa195e6af830fb7
Yemisirach <yemsrach3723@gmail.com>
2023-02-22 07:45:00 +0300

    Correct eslint error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import getScores from './Modules/getScore.js';
 import displayScores from './Modules/display.js';
 // import comments from "./Modules/comment.js";
 
-const apiId = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
+const apiId = 'https://www.themealdb.com/api/json/v1/1/search.php?f=c';
 document.querySelector('.tableBody').innerHTML = '';
 getScores(apiId).then((data) => displayScores(data.meals));
 
