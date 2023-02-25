185f7f8b53cd6310c6f479c6eeb620c7759cc6b1
Yemisirach <yemsrach3723@gmail.com>
2023-02-26 01:51:26 +0300

    correct based on review

--- src/Modules/getMeal.js
+++ src/Modules/getMeal.js
@@ -15,18 +15,19 @@ function getMealInfo(e) {
   if (/like-/.test(e.target.id)) {
     const { id } = e.target;
     const foodNum = id.match(/\d+$/)[0];
-
     addLike(foodNum);
   }
 
   if (e.target.classList.contains('comment-btn')) {
     const mealItem = e.target.parentElement.parentElement;
     fetch(
-      `https://www.themealdb.com/api/json/v1/1/categories.php?i=${mealItem.dataset.id}`,
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`,
     )
       .then((response) => response.json())
-      .then((data) => mealsFunction(data.categories));
+      .then((data) => mealsFunction(data.meals));
   }
 }
+
+// create a modal
 
 export default getMealInfo;
